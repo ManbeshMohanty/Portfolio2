5d0c79a70cdf368c469dc00b03272488270b4087
Manbesh Mohanty <manbeshmohanty@gmail.com>
2022-05-30 13:13:45 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =====================================typing animation */
+var typed= new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Datascience Aspirant"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
